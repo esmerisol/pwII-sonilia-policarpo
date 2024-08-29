df023af2225e58c1877d34f7f31633a7aad2601b
Sonilia Policarpo <sonilia.nascimento@etec.sp.gov.br>
2024-08-29 20:32:17 -0300

    aula

---
+++ Aula_03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    
+    <?php //Trazendo a função 
+    include ('funcoes_matematica.php')?>
+
+    
+    <?php //Executando a função
+     somar();
+    ComParametro(47,53);
+    ?>
+</body>
+</html>
